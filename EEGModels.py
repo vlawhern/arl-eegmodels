1b484ceb95edc23f348ac5936c39565aaf435110
robintibor <robintibor@gmail.com>
2018-06-28 15:41:45 +0200

    Fix Max Constraint for Conv Layers
    Also noted in issues at https://github.com/vlawhern/arl-eegmodels/issues/1

--- EEGModels.py
+++ EEGModels.py
@@ -302,30 +302,30 @@ def DeepConvNet(nb_classes, Chans = 64, Samples = 256,
     input_main   = Input((1, Chans, Samples))
     block1       = Conv2D(25, (1, 5), 
                                  input_shape=(1, Chans, Samples),
-                                 kernel_constraint = max_norm(2.))(input_main)
+                                 kernel_constraint = max_norm(2., axis=(0,1,2))(input_main)
     block1       = Conv2D(25, (Chans, 1),
-                                 kernel_constraint = max_norm(2.))(block1)
+                                 kernel_constraint = max_norm(2., axis=(0,1,2)))(block1)
     block1       = BatchNormalization(axis=1, epsilon=1e-05, momentum=0.1)(block1)
     block1       = Activation('elu')(block1)
     block1       = MaxPooling2D(pool_size=(1, 2), strides=(1, 2))(block1)
     block1       = Dropout(dropoutRate)(block1)
   
     block2       = Conv2D(50, (1, 5),
-                                 kernel_constraint = max_norm(2.))(block1)
+                                 kernel_constraint = max_norm(2., axis=(0,1,2)))(block1)
     block2       = BatchNormalization(axis=1, epsilon=1e-05, momentum=0.1)(block2)
     block2       = Activation('elu')(block2)
     block2       = MaxPooling2D(pool_size=(1, 2), strides=(1, 2))(block2)
     block2       = Dropout(dropoutRate)(block2)
     
     block3       = Conv2D(100, (1, 5),
-                                 kernel_constraint = max_norm(2.))(block2)
+                                 kernel_constraint = max_norm(2., axis=(0,1,2)))(block2)
     block3       = BatchNormalization(axis=1, epsilon=1e-05, momentum=0.1)(block3)
     block3       = Activation('elu')(block3)
     block3       = MaxPooling2D(pool_size=(1, 2), strides=(1, 2))(block3)
     block3       = Dropout(dropoutRate)(block3)
     
     block4       = Conv2D(200, (1, 5),
-                                 kernel_constraint = max_norm(2.))(block3)
+                                 kernel_constraint = max_norm(2., axis=(0,1,2)))(block3)
     block4       = BatchNormalization(axis=1, epsilon=1e-05, momentum=0.1)(block4)
     block4       = Activation('elu')(block4)
     block4       = MaxPooling2D(pool_size=(1, 2), strides=(1, 2))(block4)
@@ -376,9 +376,9 @@ def ShallowConvNet(nb_classes, Chans = 64, Samples = 128, dropoutRate = 0.5):
     input_main   = Input((1, Chans, Samples))
     block1       = Conv2D(40, (1, 13), 
                                  input_shape=(1, Chans, Samples),
-                                 kernel_constraint = max_norm(2.))(input_main)
+                                 kernel_constraint = max_norm(2., axis=(0,1,2)))(input_main)
     block1       = Conv2D(40, (Chans, 1), use_bias=False, 
-                          kernel_constraint = max_norm(2.))(block1)
+                          kernel_constraint = max_norm(2., axis=(0,1,2)))(block1)
     block1       = BatchNormalization(axis=1, epsilon=1e-05, momentum=0.1)(block1)
     block1       = Activation(square)(block1)
     block1       = AveragePooling2D(pool_size=(1, 35), strides=(1, 7))(block1)
